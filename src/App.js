10404dc865b9471e0ff48282e1a4b3a8c875b89f
bielCoder <gabrielhenrique9253@gmail.com>
2023-05-02 15:04:58 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
-  const name = 'Maria';
+
 
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <SayMyName name="Gabriel" />
-      <SayMyName name="João" />
-      <SayMyName name={name} />
-      <Pessoa name="Gabriel" age="24" jobs="Programador" photo="https://via.placeholder.com/150"/>
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+function Form()
+{
+    function cadastrarUsuario(e)
+    {
+        e.preventDefault();
+        console.log('Cadastrou o usuário!');
+    }
+
+    return (
+        <div>
+            <h1>Meu Cadastro:</h1>
+            <form onSubmit={ cadastrarUsuario }>
+                <div className="formGroup">
+                    <input type="text" placeholder="Digite seu nome"/>
+                </div>
+                <div className="formGroup">
+                    <input type="submit" value="Cadastrar"/>
+                </div>    
+            </form>
+        </div>
+    )
+}
+
+export default Form;
